927d492eb0a02726cc911dec67c5cd7b34817077
luisfer <luisfer_rey@yahoo.es>
2021-01-17 03:43:17 +0100

    primera app de express

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const ATLAS_DATABASE_USR=process.env.ATLAS_DATABASE_USR;
 console.log(process.env)
 console.log(ATLAS_DATABASE_USR)
 const old='mongodb://localhost/rerovaji'
-mongoose.connect('mongodb+srv://luisfer92:92Galletita@cluster0.igzkd.mongodb.net/rerovaji?retryWrites=true&w=majority', { useNewUrlParser: true ,useUnifiedTopology: true})
+mongoose.connect(old, { useNewUrlParser: true ,useUnifiedTopology: true})
 .then(db=>{
     console.log('Conexion establecida con la base de datos')
 })
